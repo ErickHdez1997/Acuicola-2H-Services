a93f15f167f8b18ddb1ad4367dc327aea2dce3ce
Erick Hernandez <Erick.Hdez199721@gmail.com>
2024-04-09 14:09:21 -0400

    Added SMTP mailing functionality using GMAIL SMTP

--- src/main/java/com/acuicola2h/monitor/controller/Controller.java
+++ src/main/java/com/acuicola2h/monitor/controller/Controller.java
@@ -1,29 +1,37 @@
 package com.acuicola2h.monitor.controller;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.acuicola2h.monitor.model.DataEntry;
 import com.acuicola2h.monitor.service.DataEntryService;
+import com.acuicola2h.monitor.service.EmailService;
 import com.acuicola2h.monitor.util.Constants;
 
-@RestController(value = Constants.CONTROLLER_PATH)
+@RestController
+@RequestMapping(Constants.CONTROLLER_PATH)
 public class Controller {
 	
 	DataEntryService dataEntryService;
+	EmailService emailService;
 	
-	public Controller(DataEntryService dataEntryService) {
+	public Controller(DataEntryService dataEntryService, EmailService emailService) {
 		this.dataEntryService = dataEntryService;
+		this.emailService = emailService;
 	}
 	
 	@PostMapping("/upload")
@@ -59,9 +67,17 @@ public class Controller {
 		return new ResponseEntity<>(dataEntryService.processDataEntry(dataEntry), HttpStatus.OK);
 	}
 	
-	@PostMapping(value = Constants.PROCESS_FILE_PATH)
+	@PostMapping(Constants.PROCESS_FILE_PATH)
 	public ResponseEntity<String> processFile() {
 		return new ResponseEntity<>("Done", HttpStatus.OK);
+	}
+	
+	@GetMapping("/sendEmail")
+	public ResponseEntity<String> sendEmail() {
+		List<String> errors = new ArrayList<>();
+		errors.add("error");
+		emailService.sendEmail(errors);
+		return new ResponseEntity<>("Email sent", HttpStatus.OK);
 	}
 	
 }
--- src/main/java/com/acuicola2h/monitor/service/EmailServiceImpl.java
+++ src/main/java/com/acuicola2h/monitor/service/EmailServiceImpl.java
@@ -1,13 +1,77 @@
 package com.acuicola2h.monitor.service;
 
 import java.util.List;
+import java.util.Properties;
 
 import org.springframework.stereotype.Service;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
+	final String from = "no-reply@Acuicola2H.com";
+    final String password = "AcNoReply!@2H";
+    
+    final String fromG = "Acuicola2H.no.reply@gmail.com";
+    final String passwordG = "nioiykrjhtmifahq";
+
+    // Recipient's email ID
+    String to = "Erick.Hdez199721@gmail.com";
+
+    // Assuming you are sending email through localhost
+    String host = "localhost"; // Or IP address/hostname of your hMailServer
+
+    // Get system properties object
+    Properties properties = System.getProperties();
+	
 	public String sendEmail(List<String> list) {
+		// hMail Server
+	    //properties.put("mail.smtp.host", host);
+	    //properties.put("mail.smtp.port", "25"); // Default SMTP port is 25, change it if you use a different one
+	    //properties.put("mail.smtp.auth", "true");
+	    //properties.put("mail.smtp.starttls.enable", "true"); // Use SSL
+	    
+	    // Gmail SMTP
+	    properties.put("mail.smtp.host", "smtp.gmail.com");
+	    properties.put("mail.smtp.port", "587");
+	    properties.put("mail.smtp.auth", "true");
+	    properties.put("mail.smtp.starttls.enable", "true"); //TLS
+	    // Get the default Session object
+	    Session session = Session.getInstance(properties, new Authenticator() {
+	        protected PasswordAuthentication getPasswordAuthentication() {
+	            return new PasswordAuthentication(fromG, passwordG);
+	        }
+	    });
+		try {
+            // Create a default MimeMessage object
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("This is the Subject Line!");
+
+            // Now set the actual message
+            message.setText("This is the actual message");
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
 		return "done";
 	}
 	
